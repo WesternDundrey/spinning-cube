e59fd1f1900e48d23eeb1e83884da1ccd44c7524
westerndundrey <hijackedpuffin@gmail.com>
2024-07-05 23:16:15 +0800

    Some more

--- cube.h
+++ cube.h
@@ -1,6 +1,11 @@
 #include <math.h>
+#include <stdio.h>
 
 float A,B,C;
+
+float cubeWidth = 10;
+int width = 160, height = 44;
+float zBuffer(160+44);
 
 float calculate(int i, int j, int k){
     return j *sin(A)*sin(B)*cos(C)-k*cos(A)*sin(B)*cos(C)+j*cos(A)*sin(C)+
@@ -10,6 +15,18 @@ float calculate(int i, int j, int k){
 
 float calculateY(int i, int j, int k) {
     return j*cos(A)*cos(C)+k*sin(A)*cos(C)-
-    j*sin(A)*sin(B)*sin(C)+k*cos(A)*sin(B)*sin(C)-i*cos(B)*sin(C)
+    j*sin(A)*sin(B)*sin(C)+k*cos(A)*sin(B)*
+    sin(C)-i*cos(B)*sin(C);
 }
-int main() { return 0; }
+
+float calculateZ(int i, int j, int k) {
+    return k*cos(A)*cos(B)-j*sin(A)*cos(B)+i*sin(B);
+}
+
+int main() { 
+    printf("\x1b[2J");
+    while(1){
+
+    }    
+    return 0;  
+}
