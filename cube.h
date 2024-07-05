bd0c1ad5c4196b3cc3938a20d1ca0ca1267fee9c
westerndundrey <hijackedpuffin@gmail.com>
2024-07-05 23:08:04 +0800

    first commit

---
+++ cube.h
@@ -0,0 +1,7 @@
+#include <math.h>
+
+float calculate(int i, int j, int k){
+    return j *sin(A)*sin(B)*cos(C)-kcos(A)*sin(B)*cos(C)+jcos(A)sin(C)+ksin(A)sin(C)+icos(B)cos(C)
+}
+
+int main() { return 0; }
