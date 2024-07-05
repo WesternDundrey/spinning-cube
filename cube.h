c3a802532e58b510fc5a93d9f08ea1ff463ed7e4
westerndundrey <hijackedpuffin@gmail.com>
2024-07-05 23:12:27 +0800

    adding in math stuff

--- cube.h
+++ cube.h
@@ -1,7 +1,15 @@
 #include <math.h>
 
+float A,B,C;
+
 float calculate(int i, int j, int k){
-    return j *sin(A)*sin(B)*cos(C)-kcos(A)*sin(B)*cos(C)+jcos(A)sin(C)+ksin(A)sin(C)+icos(B)cos(C)
+    return j *sin(A)*sin(B)*cos(C)-k*cos(A)*sin(B)*cos(C)+j*cos(A)*sin(C)+
+    k*sin(A)*sin(C)+i*cos(B)*cos(C);
 }
 
+
+float calculateY(int i, int j, int k) {
+    return j*cos(A)*cos(C)+k*sin(A)*cos(C)-
+    j*sin(A)*sin(B)*sin(C)+k*cos(A)*sin(B)*sin(C)-i*cos(B)*sin(C)
+}
 int main() { return 0; }
